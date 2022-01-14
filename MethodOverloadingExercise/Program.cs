cb811020d92eeb1654cc5865a44fda99e1d9a07f
TheMrChaptastic <chappy@themrchaptastic.com>
2022-01-14 13:37:05 -0600

    done

---
+++ MethodOverloadingExercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MethodOverloadingExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(2, 4));
+            Console.WriteLine(Add(2.2m, 4.3m));
+            Console.WriteLine(Add(5, 12, true));
+            Console.WriteLine(Add(2, 2, false));
+        }
+
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        static decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        static string Add(int a, int b, bool c)
+        {
+            return c ? $"{a + b} dollars" : "1 dollar";
+        }
+    }
+}
